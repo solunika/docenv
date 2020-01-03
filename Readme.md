0470d24a6d4f8a154289cb7827221ec780a7fbf2
Sebastian Perez <sebastiandperez37@gmail.com>
2020-01-03 15:36:25 -0300

    Error on duplicated env definition

---
+++ Readme.md
@@ -0,0 +1,14 @@
+# Docenv
+
+### Simple configuration file
+
+```
+[
+    {
+        "key": "ENVIRONMENT_VARIABLE",
+        "value": "environmentValue",
+        "help": "Message if variable not exist"
+    },
+    ...
+]
+```
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-let config = {}
+let config = {}, error = false;
 
 require('dotenv').config();
-let doc = require(__dirname+'/../../../docenv-config.json');
-if(doc === undefined) doc = []
+let doc = require(__dirname + '/../../../docenv-config.json');
+if (doc === undefined) doc = [];
 let pe = process.env;
 
 function getConfig(name, defValue, help) {
@@ -10,26 +10,30 @@ function getConfig(name, defValue, help) {
     let value = pe[name];
 
     if (!value && defValue !== undefined) {
-        console.log(`The environment variable:[ ${name} ] is not defined in .env, default value is: ${defValue}`);
+        console.warn(`The environment variable:[ ${name} ] is not defined in .env, default value is: ${defValue}`);
         return defValue;
     } else if (!value) {
         console.error("The envronment variable:[", name, "] must be defined in .env!");
         if (help) {
             console.log("Help for [", name, "]\n\t*", help);
         }
-        process.exit(1);
+        error = true;
     } else {
         return value;
     }
 }
 
 function loadConfig(varDoc) {
-    config[varDoc.key] = getConfig(varDoc.key, varDoc.value, varDoc.help)
-    //TODO: verify duplicated key
+    if (config[varDoc.key] !== undefined) {
+        console.error("Duplicated variable definition ", name, " in .env.");
+        throw new Error("Duplicated variable definition " + name + " in .env.");
+    }
+    config[varDoc.key] = getConfig(varDoc.key, varDoc.value, varDoc.help);
 }
 
 module.exports.default = function () {
     doc.forEach(loadConfig)
+    if (error) process.exit(1);
 }
 
 module.exports.Config = config;
